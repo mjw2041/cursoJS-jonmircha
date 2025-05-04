b8717d1c4ea5b3b2dad0b924113d9951d3831094
Weihmüller, Maximiliano <MWeihmuller72@hotmail.com>
2025-05-04 20:43:15 -0300

    20250504

---
+++ JS/estilosVariablesCSS.js
@@ -0,0 +1,5 @@
+console.log("Hola Mundo desde el archivo EstilosVariablesCSS.html");
+
+const $linkDOM = document.querySelector("link-dom"); // Selecciona el elemento <link-dom> del HTML
+console.log($linkDOM.style); // Muestra el objeto CSSStyleDeclaration del elemento <link-dom> en la consola
+console.log($linkDOM.getAttribute("style")); // Muestra el atributo "style" del elemento <link-dom> en la consola
