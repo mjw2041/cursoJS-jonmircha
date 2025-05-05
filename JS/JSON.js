5a97c1d6a4de9daedff6a2540f5055e47127f025
Flia Weihmuller <MWeihmuller72@hotmail.com>
2025-05-05 04:56:17 +0200

    20250504_1

--- JS/JSON.js
+++ JS/JSON.js
@@ -1,4 +1,4 @@
-const JSON = {
+const JSON_Cadena = {
     "cadena": "jon",
     "numero": 35,
     "booleano": true,
@@ -9,6 +9,34 @@ const JSON = {
     },
     "nulo": null 
 }
-
+/*
+ JSON.parse(texto) toma un string que representa datos en formato JSON (JavaScript Object Notation) 
+ y lo convierte en un valor real de JavaScript: puede ser un objeto, 
+ un array, un número, un booleano o un string.
+*/
 
 console.log(JSON)
+console.log(JSON.parse("{}"))
+console.log(JSON.parse("[1,2,3]"))
+console.log(JSON.parse("true"))
+console.log(JSON.parse("false"))
+console.log(JSON.parse("19"))
+console.log(JSON.parse("null"))
+/* console.log(JSON.parse("Hola Mundo")) */
+/* Estp da error porque no es tipo de datos aceptado 
+ *  un texto debe estar entre comillas dobles dentro del string
+*/
+console.log(JSON.parse('"Hola Mundo"'))
+
+/* 
+   JSON.stringify(valor) convierte un valor de JavaScript (objeto, array, string, número, etc.) en un texto JSON.
+   Es lo contrario de JSON.parse.
+*/
+console.log(JSON.stringify("{}"))
+console.log(JSON.stringify([1,2,3]))
+console.log(JSON.stringify(true))
+console.log(JSON.stringify(false))
+console.log(JSON.stringify(19))
+console.log(JSON.stringify(null))
+console.log(JSON.stringify("Hola Mundo"))
+console.log(JSON.stringify(JSON_Cadena))
--- JS/estilosVariablesCSS.js
+++ JS/estilosVariablesCSS.js
@@ -1,5 +1,50 @@
 console.log("Hola Mundo desde el archivo EstilosVariablesCSS.html");
 
-const $linkDOM = document.querySelector("link-dom"); // Selecciona el elemento <link-dom> del HTML
+const $linkDOM = document.querySelector(".link-dom"); // Selecciona el elemento <link-dom> del HTML
 console.log($linkDOM.style); // Muestra el objeto CSSStyleDeclaration del elemento <link-dom> en la consola
 console.log($linkDOM.getAttribute("style")); // Muestra el atributo "style" del elemento <link-dom> en la consola
+/**
+ *  la diferencia radica en  el segundo caso solo muestra las propidades que tiene valor 
+ */
+console.log($linkDOM.style.backgroundColor); //
+
+console.log(window.getComputedStyle($linkDOM)); //
+console.log(window.getComputedStyle($linkDOM).getPropertyValue("color")); //
+
+
+setTimeout(() => {
+    $linkDOM.style.setProperty("text-decoration", "none")
+    $linkDOM.style.setProperty("display", "block")
+    $linkDOM.style.width = "50%"
+    $linkDOM.style.textAlign = "center"
+    $linkDOM.style.marginLeft = "auto"
+    $linkDOM.style.marginRight = "auto"
+    $linkDOM.style.padding = "1rem"
+    $linkDOM.style.borderRadius= "0.5rem"
+    
+    console.log(" ***************** "); // Muestra el objeto CSSStyleDeclaration del elemento <link-dom> en la consola
+    console.log($linkDOM.style); // Muestra el objeto CSSStyleDeclaration del elemento <link-dom> en la consola
+    console.log(" ***************** "); // Muestra el objeto CSSStyleDeclaration del elemento <link-dom> en la consola
+    console.log($linkDOM.getAttribute("style")); // Muestra el atributo "style" del elemento <link-dom> en la consola
+
+}, 3000);
+
+// Variables CSS    -- Custom Property
+
+const $html = document.documentElement,
+      $body = document.body;
+
+let varDarkColor = getComputedStyle($html).getPropertyValue("--dark-color")
+let varYellowColor = getComputedStyle($html).getPropertyValue("--yellow-color")
+
+console.log(varDarkColor, varYellowColor)    
+
+$body.style.backgroundColor = varDarkColor
+$body.style.color = varYellowColor
+
+/* Cambiar lor valores */
+$html.style.setProperty("--dark-color", "pink")
+varDarkColor = getComputedStyle($html).getPropertyValue("--dark-color")
+console.log(varDarkColor)    
+
+$body.style.setProperty("background-color",varDarkColor)
