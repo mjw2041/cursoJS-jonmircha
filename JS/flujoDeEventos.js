1a1ff42b85246f5ad43f4534b7e06b9847c61249
Weihmüller, Maximiliano <MWeihmuller72@hotmail.com>
2025-05-28 16:47:45 -0300

    20250528

--- JS/flujoDeEventos.js
+++ JS/flujoDeEventos.js
@@ -12,13 +12,17 @@ $divEventos.forEach(div => {
       div.addEventListener("click", flujoEventos, true);
       // El tercer argumento es el uso de la burbuja o captura
       // false = burbuja      
-      // true = captura      
-      // fase = burbuja 
+      // true = captura            
       /* Otra forma de hacerlo es con el siguiente código */
       /*
       div.addEventListener("click", flujoEventos, {
             capture: false, // Fase de captura
             once: false // Solo se ejecuta una vez
       });
+
+      La diferencia entre
+        Burbuja va de evento de abajo hacia arriba ( 3,2,1)
+        captura va de evento de arriba hacia abajo ( 1,2,3)
       */
+
 });
---
+++ JS/stopPropagationPreventDefault.js
@@ -0,0 +1,21 @@
+console.log("Hola desde stopPropagationPreventDefault.js")
+
+const $divEventos = document.querySelectorAll(".eventos-flujo div"),
+   $linklEventos = document.querySelector("eventos-flujo a");
+console.log($divEventos);
+console.log($linklEventos);
+
+
+function flujoEventos(e) {
+      console.log(`Hola te saluda desde ${this.className} el click lo origino ${e.target.className}`);      
+      /*e.stopPropagation()*/ /* Para la propagacion */
+}
+
+$divEventos.forEach(div => {
+      div.addEventListener("click", flujoEventos, false);
+  
+});
+
+$linklEventos.addEventListener("click", (e) =>{
+      alert("Hola soy tu amigo y profesor Jonathan Mircha")
+})
