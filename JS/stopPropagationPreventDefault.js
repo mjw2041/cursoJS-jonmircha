dfdef8e4d185d8aaa93de2da3b2b10581882d77b
Weihmüller, Maximiliano <MWeihmuller72@hotmail.com>
2025-05-30 11:13:25 -0300

    20252530

--- JS/stopPropagationPreventDefault.js
+++ JS/stopPropagationPreventDefault.js
@@ -1,15 +1,15 @@
 console.log("Hola desde stopPropagationPreventDefault.js")
 
 const $divEventos = document.querySelectorAll(".eventos-flujo div"),
-   $linklEventos = document.querySelector("eventos-flujo a");
+   $linklEventos = document.querySelector(".eventos-flujo a");
 console.log($divEventos);
 console.log($linklEventos);
 
 
 function flujoEventos(e) {
       console.log(`Hola te saluda desde ${this.className} el click lo origino ${e.target.className}`);      
-      /*e.stopPropagation()*/ /* Para la propagacion */
-}
+      e.stopPropagation() /* Para la propagacion */
+} 
 
 $divEventos.forEach(div => {
       div.addEventListener("click", flujoEventos, false);
@@ -18,4 +18,6 @@ $divEventos.forEach(div => {
 
 $linklEventos.addEventListener("click", (e) =>{
       alert("Hola soy tu amigo y profesor Jonathan Mircha")
+      e.preventDefault(); /* Cambiar la accion por defecto a la que nosotros necesitamos */
+      e.stopPropagation();
 })
